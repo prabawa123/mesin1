4c0feb5623417ffdcca4f15b5340e3d90055ef1e
prabawa123 <84450725+prabawa123@users.noreply.github.com>
2021-05-19 19:03:02 +0700

    Create nicehash.sh

---
+++ nicehash.sh
@@ -0,0 +1,5 @@
+# nicehash
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u 3HKVL5u7KECJTXXgNxd5nSLDbaFooqEJLE.mesin1 -p x --cpu 4
